a7de8d036ca6216334058fcc994c5ef885a5854e
alvar <alvar@DESKTOP-GDQ98VF>
2022-07-26 08:53:18 +0200

    D02 - Task09: Completada

---
+++ src/main/java/acme/forms/EpicureDashboard.java
@@ -0,0 +1,27 @@
+package acme.forms;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.springframework.data.util.Pair;
+
+import acme.datatypes.StatData;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EpicureDashboard implements Serializable{
+
+		private static final long					serialVersionUID	= 1L;
+		
+		Integer											numDishesProposed;
+		Integer											numDishesAccepted;
+		Integer											numDishesDenied;
+		
+		
+		
+		//StatData=[Sum, Average, Desviation, Minimum, Maximum] by currency
+
+		Map<Pair<DishStatus, String>, StatData>	dishesBudgetData;
+}
