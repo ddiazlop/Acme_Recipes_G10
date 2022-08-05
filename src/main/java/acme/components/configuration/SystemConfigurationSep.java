293ea0117dc436bdd9f3892fff890c06c8a37166
Diego <diewentdiaz@gmail.com>
2022-08-05 12:29:50 +0200

    Fixes + Sample Data
    - Fixed enum type of "wareType" from KitchenWare.java
    - Added Sample data for Ingredients, Kitchen Utensils, Chefs, user-accounts

--- src/main/java/acme/components/configuration/SystemConfigurationSep.java
+++ src/main/java/acme/components/configuration/SystemConfigurationSep.java
@@ -1,15 +1,8 @@
 package acme.components.configuration;
 
-import java.util.Map;
-
 import javax.persistence.Entity;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
-import org.dom4j.tree.AbstractEntity;
-import org.hibernate.validator.constraints.Range;
-
+import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 @Entity
@@ -17,29 +10,29 @@ import lombok.Setter;
 @Setter
 public class SystemConfigurationSep extends AbstractEntity{
 
-	
-		// Serialisation identifier -----------------------------------------------
-
-		protected static final long	serialVersionUID	= 1L;
-
-		// Attributes -------------------------------------------------------------
-		@NotBlank
-		protected String			systemCurrency;
-
-		@NotBlank
-		@Pattern(regexp = "^[A-Z]{3}(,[A-Z]{3})*$")
-		protected String			acceptedCurrencies;
-
-		@NotBlank
-		protected Map<String,Double>	SpamTuple;
-
-		//Supuestamente el rango es de 0.00 a 1.00, por comodidad de 0 a 100
-		@Range(max = 100, min = 0)
-		@NotNull
-		protected Double			SpamThreshold;
-
-		// Derived attributes -----------------------------------------------------
-
-		// Relationships ----------------------------------------------------------
+// TODO DEPRECATED, NEEDS TO BE CHANGED
+//		// Serialisation identifier -----------------------------------------------
+//
+//		protected static final long	serialVersionUID	= 1L;
+//
+//		// Attributes -------------------------------------------------------------
+//		@NotBlank
+//		protected String			systemCurrency;
+//
+//		@NotBlank
+//		@Pattern(regexp = "^[A-Z]{3}(,[A-Z]{3})*$")
+//		protected String			acceptedCurrencies;
+//
+////		@NotBlank
+////		protected Map<String,Double>	SpamTuple;
+//
+//		//Supuestamente el rango es de 0.00 a 1.00, por comodidad de 0 a 100
+//		@Range(max = 100, min = 0)
+//		@NotNull
+//		protected Double			SpamThreshold;
+//
+//		// Derived attributes -----------------------------------------------------
+//
+//		// Relationships ----------------------------------------------------------
 
 }
