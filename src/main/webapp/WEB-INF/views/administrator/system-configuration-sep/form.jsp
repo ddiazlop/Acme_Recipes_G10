93be4810cc3f47c06d822ee000b19c961766f69f
alvar <alvar@DESKTOP-GDQ98VF>
2022-08-12 18:43:17 +0200

    D03-Task16 Completada, añadidas vistas, features y test (los casos positivos y las pruebas de hackeo funcionan correctamente)

--- src/main/webapp/WEB-INF/views/administrator/system-configuration-sep/form.jsp
+++ src/main/webapp/WEB-INF/views/administrator/system-configuration-sep/form.jsp
@@ -4,32 +4,48 @@
 <%@taglib prefix="acme" uri="urn:jsptagdir:/WEB-INF/tags"%>
 
 <acme:form>	
-	<acme:button code="administrator.system-configuration.button.title.accepted-currencies" action="/administrator/system-configuration-sep/accepted-currencies"/>
-	<acme:button code="administrator.system-configuration.button.title.system-currency" action="/administrator/system-configuration-sep/system-currency"/>
-	<acme:button code="administrator.system-configuration.button.title.spam" action="/administrator/system-configuration-sep/spam"/>
+	<acme:button code="administrator.system-configuration-sep.button.title.accepted-currencies" action="/administrator/system-configuration-sep/accepted-currencies"/>
+	<acme:button code="administrator.system-configuration-sep.button.title.system-currency" action="/administrator/system-configuration-sep/system-currency"/>
+	<acme:button code="administrator.system-configuration-sep.button.title.spam" action="/administrator/system-configuration-sep/spam"/>
 	
 	<h1>
 	</h1>
 	<jstl:choose>
 	
 		<jstl:when test="${command == 'accepted-currencies' }">
-			<acme:input-textbox code="administrator.system-configuration.form.label.acceptedCurrencies" path="acceptedCurrencies"/>
+			<h2>
+				<acme:message code="administrator.system-configuration.form.title.acceptedCurrencies"/>
+			</h2>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.acceptedCurrencies" path="acceptedCurrencies"/>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.systemCurrency" path="systemCurrency" readonly="true"/>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.spamTuple" path="spamTuple" readonly="true"/>
+			<acme:input-double code="administrator.system-configuration-sep.form.label.spamThreshold" path="spamThreshold" readonly="true"/>
+			
+		
 		</jstl:when>
 		
 		<jstl:when test="${command == 'system-currency' }">
-			<acme:input-select code="administrator.system-configuration.form.label.systemCurrency" path="systemCurrency">
+			<h2>
+				<acme:message code="administrator.system-configuration.form.title.systemCurrency"/>
+			</h2>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.acceptedCurrencies" path="acceptedCurrencies" readonly="true"/>
+			<acme:input-select code="administrator.system-configuration-sep.form.label.systemCurrency" path="systemCurrency">
                 <jstl:forEach items="${accepted}" var="acceptedCurr">
                     <acme:input-option code="${acceptedCurr}" value="${acceptedCurr}"/>
                 </jstl:forEach>
 			</acme:input-select>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.spamTuple" path="spamTuple" readonly="true"/>
+			<acme:input-double code="administrator.system-configuration-sep.form.label.spamThreshold" path="spamThreshold" readonly="true"/>
 		</jstl:when>
 		
 		<jstl:when test="${command == 'spam' }">
 			<h2>
 				<acme:message code="administrator.system-configuration.form.title.spam"/>
 			</h2>
-			<acme:input-textbox code="administrator.system-configuration.form.label.spamTuple" path="spamTerms"/>
-			<acme:input-double code="administrator.system-configuration.form.label.spamThreshold" path="spamThreshold"/>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.acceptedCurrencies" path="acceptedCurrencies" readonly="true"/>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.systemCurrency" path="systemCurrency" readonly="true"/>
+			<acme:input-textbox code="administrator.system-configuration-sep.form.label.spamTuple" path="spamTuple"/>
+			<acme:input-double code="administrator.system-configuration-sep.form.label.spamThreshold" path="spamThreshold"/>
 		</jstl:when>
 	</jstl:choose>
 	
---
+++ src/test/java/acme/testing/administrator/systemConfigurationSep/AdministratorSystemConfigurationSepUpdateTest.java
@@ -0,0 +1,117 @@
+package acme.testing.administrator.systemConfigurationSep;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
+
+import acme.framework.testing.BrowserDriver;
+import acme.testing.TestHarness;
+
+public class AdministratorSystemConfigurationSepUpdateTest extends TestHarness{
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/systemConfigurationSep/update-system-configuration-sep.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex, final String systemCurrency, final String acceptedCurrencies, final String spamTuple, final String spamThreshold) {
+		super.signIn("administrator", "administrator");
+
+		super.clickOnMenu("Administrator", "Update System Details Sep");
+		super.checkFormExists();
+		
+		super.clickOnButton("Accepted currencies");
+		super.checkFormExists();
+		super.fillInputBoxIn("acceptedCurrencies", acceptedCurrencies);
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+		
+		super.clickOnButton("System currency");
+		super.checkFormExists();
+		final BrowserDriver driver = super.getDriver();
+		driver.locateOne(By.xpath("//*[@name=\"systemCurrency_proxy\"]/option[\""+ systemCurrency +"\"]")).click();
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+		
+		super.clickOnButton("Spam Words");
+		super.checkFormExists();
+		super.fillInputBoxIn("spamTuple", spamTuple);
+		super.fillInputBoxIn("spamThreshold", spamThreshold);
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+
+		super.clickOnMenu("Administrator", "Update System Details Sep");
+		super.checkFormExists();
+		
+		super.clickOnButton("Accepted currencies");
+		super.checkInputBoxHasValue("acceptedCurrencies", acceptedCurrencies);
+		
+		super.clickOnButton("Spam Words");
+		super.checkInputBoxHasValue("spamTuple", spamTuple);
+		super.checkInputBoxHasValue("spamThreshold", spamThreshold);
+		
+		super.checkNotErrorsExist();
+
+		super.signOut();
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/systemConfigurationSep/update-system-configuration-sep-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void negativeTest(final int recordIndex, final String systemCurrency, final String acceptedCurrencies, final String spamTuple, final String spamThreshold) {
+		super.signIn("administrator", "administrator");
+
+		super.clickOnMenu("Administrator", "Update System Details Sep");
+		super.checkFormExists();
+		
+		super.clickOnButton("Accepted currencies");
+		super.checkFormExists();
+		super.fillInputBoxIn("acceptedCurrencies", acceptedCurrencies);
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+		
+		super.clickOnButton("System currency");
+		final BrowserDriver driver = super.getDriver();
+		driver.locateOne(By.xpath("//*[@name=\"systemCurrency_proxy\"]/option[\""+ systemCurrency +"\"]")).click();
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+		
+		super.clickOnButton("Spam Words");
+		super.fillInputBoxIn("spamTuple", spamTuple);
+		super.fillInputBoxIn("spamThreshold", spamThreshold);
+		super.clickOnSubmit("Update");
+		super.checkNotErrorsExist();
+
+		super.clickOnMenu("Administrator", "Update System Details Sep");
+		super.checkFormExists();
+		
+		super.clickOnButton("Accepted currencies");
+		super.checkInputBoxHasValue("acceptedCurrencies", acceptedCurrencies);
+		
+		super.clickOnButton("Spam Words");
+		super.checkInputBoxHasValue("spamTuple", spamTuple);
+		super.checkInputBoxHasValue("spamThreshold", spamThreshold);
+		super.checkErrorsExist();
+		
+		super.signOut();
+	}
+	
+	@Test
+	@Order(30)
+	public void hackingTest() {
+		
+		super.navigate("/administrator/system-configuration-sep/update");
+		super.checkPanicExists();
+		
+		super.signIn("chef1", "chef1");
+		super.navigate("/administrator/system-configuration-sep/update");
+		super.checkPanicExists();
+		super.signOut();
+
+		super.signIn("epicure1", "epicure1");
+		super.navigate("/administrator/system-configuration-sep/update");
+		super.checkPanicExists();
+		super.signOut();
+	}
+	
+}
