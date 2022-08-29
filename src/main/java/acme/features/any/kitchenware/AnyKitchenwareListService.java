ea2eacfd1a1211b80791800b16b70b8a08d46c8e
alvar <alvar@DESKTOP-GDQ98VF>
2022-08-29 14:58:46 +0200

    Ya vuelve a estar kitchenware

---
+++ src/main/java/acme/features/any/kitchenware/AnyKitchenwareListService.java
@@ -0,0 +1,51 @@
+package acme.features.any.kitchenware;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.recipes.Kitchenware;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Any;
+import acme.framework.services.AbstractListService;
+
+@Service
+public class AnyKitchenwareListService implements AbstractListService<Any, Kitchenware>{
+
+	@Autowired
+	protected AnyKitchenwareRepository repo;
+
+	@Override
+	public boolean authorise(final Request<Kitchenware> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public Collection<Kitchenware> findMany(final Request<Kitchenware> request) {
+		assert request != null;
+		Collection<Kitchenware> res;
+		if (request.isCommand("list-ingredient")) {
+			res = this.repo.findPublishedIngredients();
+		}else {
+			res = this.repo.findAllPublishedUtensils();
+		}
+
+		return res;
+	}
+
+	@Override
+	public void unbind(final Request<Kitchenware> request, final Kitchenware entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "code", "name", "published");
+
+	}
+
+
+
+}
---
+++ src/main/java/acme/features/any/kitchenware/AnyKitchenwareShowService.java
@@ -0,0 +1,56 @@
+package acme.features.any.kitchenware;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.recipes.Kitchenware;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Any;
+import acme.framework.services.AbstractShowService;
+
+@Service
+public class AnyKitchenwareShowService implements AbstractShowService<Any, Kitchenware>{
+
+	@Autowired
+	protected AnyKitchenwareRepository repo;
+
+	@Override
+	public boolean authorise(final Request<Kitchenware> request) {
+		assert request != null;
+		int ingredientId;
+		Kitchenware ingredient;
+		boolean res;
+
+		ingredientId = request.getModel().getInteger("id");
+		ingredient = this.repo.findOneKitchenwareById(ingredientId);
+		res = ingredient.isPublished();
+		return res;
+	}
+
+	@Override
+	public Kitchenware findOne(final Request<Kitchenware> request) {
+		assert request != null;
+		int ingredientId;
+		Kitchenware ingredient;
+
+		ingredientId = request.getModel().getInteger("id");
+		ingredient = this.repo.findOneKitchenwareById(ingredientId);
+
+		return ingredient;
+	}
+
+	@Override
+	public void unbind(final Request<Kitchenware> request, final Kitchenware entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "code", "name", "wareType", "description", "retailPrice", "info", "published");
+
+
+	}
+
+
+
+}
