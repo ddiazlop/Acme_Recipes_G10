e7f7147374790a2b603d361b7da3f856151715f7
Diego <diewentdiaz@gmail.com>
2022-09-07 19:33:17 +0200

    Some fixes to tests,epicureMemoranda fails

--- src/test/java/acme/testing/epicure/fineDish/EpicureFineDishListAndShowTest.java
+++ src/test/java/acme/testing/epicure/fineDish/EpicureFineDishListAndShowTest.java
@@ -1,28 +1,24 @@
 
 package acme.testing.epicure.fineDish;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import acme.framework.datatypes.Money;
 import acme.testing.TestHarness;
 
-public class EpicureFineDishListAndShow extends TestHarness {
+public class EpicureFineDishListAndShowTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/epicure/fineDish/list-and-show-test.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveRecipeTest(final int recordIndex, final String code, final String request, final String budget, 
-		final String creationDate, final String startDate,final String endDate, final String info, final String state) {
+		final String creationDate, final String startDate,final String endDate, final String info) {
 		
 		super.signIn("epicure1", "epicure1");
 		super.navigateHome();
 		
-		super.clickOnMenu("FineDish", "FineDIsh");
+		super.clickOnMenu("Epicure", "My Fine Dishes");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
@@ -33,7 +29,6 @@ public class EpicureFineDishListAndShow extends TestHarness {
 		super.checkColumnHasValue(recordIndex, 4, startDate);
 		super.checkColumnHasValue(recordIndex, 5, endDate);
 		super.checkColumnHasValue(recordIndex, 6, info);
-		super.checkColumnHasValue(recordIndex, 7, state);
 		
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
--- src/test/java/acme/testing/epicure/memoranda/EpicureMemorandaListAndShowTest.java
+++ src/test/java/acme/testing/epicure/memoranda/EpicureMemorandaListAndShowTest.java
@@ -1,24 +1,23 @@
 
-package acme.testing.epicure.memorandum;
+package acme.testing.epicure.memoranda;
 
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class EpicureMemorandumListAndShow extends TestHarness {
+public class EpicureMemorandaListAndShowTest extends TestHarness {
 	@ParameterizedTest
-	@CsvFileSource(resources = "/epicure/memodandum/list-and-show-test.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/epicure/memorandum/list-and-show-test.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void positiveRecipeTest(final int recordIndex, final String code, final String report, final String moment
-		, final String info, final String state) {
+		, final String info) {
 		
 		super.signIn("epicure1", "epicure1");
 		super.navigateHome();
 		
-		super.clickOnMenu("Memorandum", "Memorandum");
+		super.clickOnMenu("Epicure", "My Memoranda");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 		
@@ -26,7 +25,6 @@ public class EpicureMemorandumListAndShow extends TestHarness {
 		super.checkColumnHasValue(recordIndex, 1, moment);
 		super.checkColumnHasValue(recordIndex, 2, report);
 		super.checkColumnHasValue(recordIndex, 3, info);
-		super.checkColumnHasValue(recordIndex, 4, state);
 		
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
